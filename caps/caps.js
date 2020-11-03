4496667da1106eac1d17c09b87d02e8590c83904
bayanalalem <bayanalalem0@gmail.com>
2020-11-04 00:39:53 +0200

    done

--- caps/caps.js
+++ caps/caps.js
@@ -1,43 +1,91 @@
-const net = require('net');
 
-const uuidv4 = require('uuid').v4;
-require('dotenv').config()
-const PORT = process.env.PORT || 4000;
+  
+const io = require('socket.io')(6000); //1
 
-const server = net.createServer();
-server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+const caps = io.of('/caps');
+let currentRoom = '';
+caps.on('connection', (socket) => {
+  
+  console.log('Welcome to the caps chat Server!', socket.id);
+  socket.on('join', (room) => {
+    console.log('Joined a new Room', room)
+    socket.leave(currentRoom); 
+    socket.join(room);
+    currentRoom = room;
+  });
 
-const socketPool = {};
-server.on('connection', (socket) => {
-  console.log('Socket Connected!');
-  const id = `socket-${uuidv4()}`;
-  socketPool[id] = socket;
-  socket.on('data', (buffer) => dispatchEvent(buffer));
-  socket.on('error', (e) => console.log('SOCKET ERROR', e.message));
-  socket.on('end', (id) => delete socketPool[id]);
+ 
+      socket.on('pickup', (payload) => {
+        log('pickup', payload)
+        caps.emit('pickup', payload);
+        
+      });
+      socket.on('in-transit', (payload) => {
+        
+        log('in-transit', payload)
+        // caps.emit('pickup', payload);
+      });
+      socket.on('delivered', (payload) => {
+        console.log("ggg")
+        log('delivered', payload)
+        caps.to(currentRoom).emit('delivered', payload);
+        
+       });
+  
+      //  socket.on('delivered', (payload) => {
+      //   console.log("ggg")
+      //   log('delivered', payload)
+      //    caps.to(currentRoom).emit('delivered', payload);
+        
+      //  });
 });
-server.on('error', (e) => console.log('SERVER ERROR', e.message));
 
 
-function dispatchEvent(buffer) {
-  for (let socket in socketPool) {
-    socketPool[socket].write(buffer); //6
-  }
 
-  let dtobj = JSON.parse(buffer.toString().trim());
-  if (dtobj.event == 'pickup') {
-    log('pickup', dtobj);
-  }
-  if (dtobj.event == 'in-transit') {
-    log('in-transit', dtobj);
-  }
-  if (dtobj.event == 'delivered') {
-    log('delivered', dtobj);
-  }
 
-}
+
+
+// const net = require('net');
+
+// const uuidv4 = require('uuid').v4;
+// require('dotenv').config()
+// const PORT = process.env.PORT || 4000;
+
+// const server = net.createServer();
+// server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+// const socketPool = {};
+// server.on('connection', (socket) => {
+//   console.log('Socket Connected!');
+//   const id = `socket-${uuidv4()}`;
+//   socketPool[id] = socket;
+//   socket.on('data', (buffer) => dispatchEvent(buffer));
+//   socket.on('error', (e) => console.log('SOCKET ERROR', e.message));
+//   socket.on('end', (id) => delete socketPool[id]);
+// });
+// server.on('error', (e) => console.log('SERVER ERROR', e.message));
+
+
+// function dispatchEvent(buffer) {
+//   for (let socket in socketPool) {
+//     socketPool[socket].write(buffer); //6
+//   }
+
+//   let dtobj = JSON.parse(buffer.toString().trim());
+//   if (dtobj.event == 'pickup') {
+//     log('pickup', dtobj);
+//   }
+//   if (dtobj.event == 'in-transit') {
+//     log('in-transit', dtobj);
+//   }
+//   if (dtobj.event == 'delivered') {
+//     log('delivered', dtobj);
+//   }
+
+// }
 
 
 function log(event, payload) {
+  
   console.log({ event, time: new Date(), payload });
 }
--- vender/vendor.js
+++ vender/vendor.js
@@ -1,44 +1,106 @@
 
-const inquirer = require('inquirer'); // library to get user input
-
-const net = require('net'); // TCP library built into node
+// const inquirer = require('inquirer');
+const io = require('socket.io-client');
+const caps = io.connect('http://localhost:6000/caps'); 
 let faker = require('faker');
-const client = new net.Socket();
-require('dotenv').config()
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 4000;
-let STORENAME = process.env.STORENAME ||faker.commerce.product() ;
 
-let name = '';
-const messages = [];
-client.connect(PORT, HOST, () => {
+caps.on('connect', () => {
+
+// console.log("dd",caps)
+// let STORENAME = faker.commerce.product() ;
+// caps.emit('join', STORENAME); 
   console.log('Client Connected');
-  setInterval(()=>{
-    let obj = {
-        storeName:`${STORENAME}`,
-        orderID: `${faker.random.uuid()}`,
-        custome: `${faker.name.findName()}`,
-        address:`${faker.address.streetName()}`
-      }; 
-      let massege = JSON.stringify({ event:'pickup' , payload: obj });
+    setInterval(()=>{
+      let obj = {
+          storeName:`${faker.commerce.product()}`,
+          orderID: `${faker.random.uuid()}`,
+          custome: `${faker.name.findName()}`,
+          address:`${faker.address.streetName()}`
+        }; 
+        caps.emit('join', obj.storeName); 
+        caps.emit('pickup', obj);
+   
+      },5000)
+          caps.on('delivered', (payload) => {
+            console.log(`Thank you for delivering ${payload.orderID}`)
 
-      client.write(massege);
+           });
+      
+
+          // client.on('data', (data) => {
+              
+          //   const dataObj = JSON.parse(data);
+         
+          //   let i=dataObj.payload.orderID
+          //   if(dataObj.event=='delivered'){
+          //     console.log(` Thank you for delivering ${i}`)
+          //   };
+          // });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inquirer = require('inquirer'); // library to get user input
+
+// const net = require('net'); // TCP library built into node
+// let faker = require('faker');
+// const client = new net.Socket();
+// require('dotenv').config()
+// const HOST = process.env.HOST || 'localhost';
+// const PORT = process.env.PORT || 4000;
+// let STORENAME = process.env.STORENAME ||faker.commerce.product() ;
+
+// let name = '';
+// const messages = [];
+// client.connect(PORT, HOST, () => {
+//   console.log('Client Connected');
+//   setInterval(()=>{
+//     let obj = {
+//         storeName:`${STORENAME}`,
+//         orderID: `${faker.random.uuid()}`,
+//         custome: `${faker.name.findName()}`,
+//         address:`${faker.address.streetName()}`
+//       }; 
+//       let massege = JSON.stringify({ event:'pickup' , payload: obj });
+
+//       client.write(massege);
      
     
       
-    },5000)
-        client.on('data', (data) => {
+//     },5000)
+//         client.on('data', (data) => {
             
-          const dataObj = JSON.parse(data);
+//           const dataObj = JSON.parse(data);
        
-          let i=dataObj.payload.orderID
-          if(dataObj.event=='delivered'){
-            console.log(` Thank you for delivering ${i}`)
-          };
-        });
-    client.on('close', () => console.log('Connection closed!'));
-    client.on('error', (err) => console.log('Logger Error', err.message));
-  });
+//           let i=dataObj.payload.orderID
+//           if(dataObj.event=='delivered'){
+//             console.log(` Thank you for delivering ${i}`)
+//           };
+//         });
+//     client.on('close', () => console.log('Connection closed!'));
+//     client.on('error', (err) => console.log('Logger Error', err.message));
+//   });
 
 
 
